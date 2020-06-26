c1510033830ef51642223943aa226f1812a8277d
ackermannQ <ackermann.quentin@gmail.com>
2020-06-26 09:38:57 +0200

    Building backend Spring

---
+++ backend/src/test/java/com/lunchz/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
